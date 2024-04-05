a81bd5163b4fd012c3244d8f386cf227b780961e
douvewane <douvewane85@gmail.com>
2024-04-05 18:14:50 +0000

    COURS DE JAVA

---
+++ gesbien/src/Operation.java
@@ -0,0 +1,13 @@
+public class Operation {
+
+    public static int somme(int a,int b){
+        return a+b;
+    }
+    public static double somme(double a,int b){
+        return a+b;
+    }
+    public static int somme(int a,int b,int c){
+        return a+b+c;
+    }
+    
+}
